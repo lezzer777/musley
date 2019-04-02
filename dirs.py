37bfe527705cfdce069364cb4c2607cd73cbd260
lezzer777 <lezzer777@gmail.com>
2019-04-02 19:51:29 +0300

    normal exit && files

--- dirs.py
+++ dirs.py
@@ -7,9 +7,21 @@ class dir:
 		self.subdirs = subdirs
 		self.files = files		
 
+class file:
+	name = ""
+	content = ""
+	type = ""
+	def __init__(self, name, content, type):
+		self.name = name
+		self.content = content
+		self.type = type
+# Files initialization
+
+readme = file("readme.txt", "this is file for test bla-bla-bla", "txt") 
+
 # Directories initialization sub --> parents
 
-userdir = dir("~", [], [])
+userdir = dir("~", [], [readme])
 home = dir("home", [userdir], [])
 
 root = dir("/", [home], []) # Always at the end
--- musley.py
+++ musley.py
@@ -1,11 +1,17 @@
 from termcolor import colored
 import dirs
+import sys
 
 session = {
 	"user": "user",
 	"cur_dir": dirs.userdir
 }
 
-
 while True:
-	comd = input("[" + session["user"] + colored("@", "red") + "pc] ")
+	try:
+		comd = input("[" + session["user"] + colored("@", "red") + "pc] ")
+	except KeyboardInterrupt:
+		print("please, use exit command to exit.")
+
+	if comd == "exit":
+		sys.exit("Goodbye, " + session["user"])
