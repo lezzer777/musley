a584443acc7c47e2a02398d72b254d5819965b24
Ввлерий а <tvsclass@gmail.com>
2019-04-04 01:03:56 +0300

    modified:   libtvs.py

--- libtvs.py
+++ libtvs.py
@@ -1,25 +1,26 @@
+import tvslogo
 from termcolor import colored
 from os import system
 
 def red(what):
-        a=colored(what,'red')
-        return a
+ a=colored(what,'red')
+ return a
 def blue(what):
-        a=colored(what,'blue')
-        return a
+ a=colored(what,'blue')
+ return a
 def green(what):
-        a=colored(what,'green')
-        return a
+ a=colored(what,'green')
+ return a
 def yellow(what):
-        a=colored(what,'yellow')
-        return a
+ a=colored(what,'yellow')
+ return a
 def purple(what):
-        a=colored(what,'magenta')
-        return a
+ a=colored(what,'magenta')
+ return a
 def ok(what):
-	print('[' + green(OK) + '] ' + what)
+	print('[' + green('OK') + '] ' + what)
 def error(what):
-	print('[' + red(ERROR) + '] ' + what)
+	print('[' + red('ERROR') + '] ' + what)
 def logo():
 	print(red("""
 @@  @@  @@  @@ @@@@@ @@   @@@@@ @@  @@
