6158dc24c357c6b25771ec2d3b1f5475b6d07908
Ввлерий а <tvsclass@gmail.com>
2019-04-05 12:16:57 +0300

    dialogus.py upgraded

--- dialogus.py
+++ dialogus.py
@@ -1,11 +1,47 @@
 import libtvs
 
+class somebody:
+	name=''
+	unreaded=''
+	history=''
+	todo=''
+	def __init__(self, name, unreaded, history, todo):
+		self.name=name
+		self.unreaded=unreaded
+		self.history=history
+		self.todo=todo
+
+test=somebody('sunshine',None,None,None)
+
+def viewhistory(fromwho):
+	print('========================================')
+	print(eval(fromwho).history)
+
+def viewmessage(fromwho):
+	if eval(fromwho).unread != None:
+		print(eval(fromwho).unread)
+
+		if eval(fromwho).todo != None:
+			c=eval(fromwho).todo
+			eval(c)
+			eval(fromwho).todo=None
+
+		try:
+			eval(fromwho).history=eval(fromwho).history + '\n' + eval(fromwho).unread
+		except TypeError:
+			eval(fromwho).history=eval(fromwho).unread
+		eval(fromwho).unread=None
+	else:
+		print('empty')
+
 def say(name, phrase):
 	print(libtvs.blue(name) + ": " + phrase)
 
-def notify(name, phrase):
-	print("You have an unread message from: " + libtvs.blue(name))
-	print(libtvs.blue(name) + ": " + phrase)
+def notify(name, phrase, todo):
+	
+	print("You have an unread message from: " + libtvs.blue(eval(name).name))
+	eval(name).unread=(libtvs.blue(eval(name).name) + ": " + phrase)
+	eval(name).todo=todo
 
 def menu(answers):
 	for answer in answers:
