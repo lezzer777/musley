12019f105ed1de1ebb4382b4faff3e9531c59ef2
lezzer777 <lezzer777@gmail.com>
2019-04-05 23:10:01 +0300

    libsoft updated

--- libsoft.py
+++ libsoft.py
@@ -15,16 +15,27 @@ class addr:
 
 # Initializtation
 evilcorp = addr(["tcp", "smb"], "228.6.6.31", True)
+adresses = [evilcorp]
 
-def nmap(addr):
+def nmap(ip):
+    obj = None
     print("Starting Nmap 7.70 ( https://nmap.org ) at " + str(datetime.date.today()))
-    time. sleep(4)
-    print("Nmap scan report for " + addr.ip)
-    print("Host is up (" + str(random.random()) + " lattency).")
-    print("Not shown: " + str(1000 - len(addr.openPorts)) + " closed ports")
-    print("PORT    STATE    SERVICE")
-    if "smb" in addr.openPorts:
-        print("139/smb   open    windows")
-    if "tcp" in addr.openPorts:
-        print("8080      open    whoknows")
-nmap(evilcorp)
+    time.sleep(4)
+
+    for adress in adresses:
+        if ip == adress.ip:
+            obj  = adress
+    
+    if obj != None:
+        print("Nmap scan report for " + ip)
+        print("Host is up (" + str(random.random()) + " lattency).")
+        print("Not shown: " + str(1000 - len(adress.openPorts)) + " closed ports")
+        print("PORT    STATE    SERVICE")
+        if "smb" in adress.openPorts:
+            print("139/smb   open    windows")
+        if "tcp" in adress.openPorts:
+            print("8080      open    whoknows")
+    else:
+        print("Host is down")
+nmap("228.6.6.31")
+nmap("flex")
