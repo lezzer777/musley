325df3157e59f65c836b6f45b004af8cffc2dd80
Ввлерий а <tvsclass@gmail.com>
2019-04-05 16:02:28 +0300

    dialogus bugfix

--- dialogus.py
+++ dialogus.py
@@ -1,4 +1,5 @@
 import libtvs
+from libtvs import green
 
 class somebody:
 	name=''
@@ -23,40 +24,45 @@ def viewmessage(fromwho):
 
 		if eval(fromwho).unreaded != None:
 			print(eval(fromwho).unreaded)
-
+			print(eval(fromwho).name)
 			if eval(fromwho).todo != None:
 				c=eval(fromwho).todo
 				eval(c)
 				eval(fromwho).todo=None
 			try:
-				eval(fromwho).history=eval(fromwho).history + '\n' + eval(fromwho).unread
+				eval(fromwho).history=eval(fromwho).history + '\n' + eval(fromwho).unreaded
 			except TypeError:
-				eval(fromwho).history=eval(fromwho).unread
+				eval(fromwho).history=eval(fromwho).unreaded
 			eval(fromwho).unreaded=None
 		else:
 			print('empty')
 	except NameError:
-		print('error03: no such person')
+		print('error03: ' + fromwho + ': no such person')
 
 
 def say(name, phrase):
 	print(libtvs.blue(name) + ": " + phrase)
 
 def notify(name, phrase, todo):
-	
 	print("You have an unread message from: " + libtvs.blue(eval(name).name) + '\n type \'qms view ' + eval(name).name + '\' to open message')
-	eval(name).unread=(libtvs.blue(eval(name).name) + ": " + phrase)
+	eval(name).unreaded=(libtvs.blue(eval(name).name) + ": " + phrase)
 	eval(name).todo=todo
 
-def menu(answers):
+def menu(answers, asking):
 	for answer in answers:
 		print(str(answers.index(answer) + 1) + ") " + answer)
 	callback = ""
 	while callback == "1" or callback != "2":
 		callback = input("Answer: ")
 		if callback == "1":
+			you=green('You: ')
+			eah=eval(asking).history
+			eval(asking).history=(str(you) + str(eah) + str(answers[0]))
 			return 1
 		elif callback == "2":
+			you=green('You: ')
+			eah=eval(asking).history
+			eval(asking).history=(str(you) + str(eah) + str(answers[1]))
 			return 2
 		else:
 			print(libtvs.red("Not right number, yeah?"))
