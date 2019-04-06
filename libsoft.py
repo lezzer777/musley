395c0818e949d9105c07f1573a5f8610e9bd6713
lezzer777 <lezzer777@gmail.com>
2019-04-06 18:31:54 +0300

    simple ssh system

--- libsoft.py
+++ libsoft.py
@@ -7,14 +7,16 @@ class addr:
     openPorts = []
     ip = ""
     isActive = False
-
-    def __init__(self, openPorts, ip, isActive):
+    rootPasswd = ""
+    hostname = ""
+    def __init__(self, openPorts, ip, isActive, rootPasswd, hostname):
         self.openPorts = openPorts
         self.ip = ip
         self.isActive = isActive
-
+        self.rootPasswd = rootPasswd
+        self.hostname = hostname
 # Initializtation
-evilcorp = addr(["tcp", "smb"], "228.6.6.31", True)
+evilcorp = addr(["tcp", "smb", "ssh"], "228.6.6.31", True, "123", "evil")
 adresses = [evilcorp]
 
 def nmap(ip):
@@ -35,5 +37,27 @@ def nmap(ip):
             print("139/smb   open    windows")
         if "tcp" in adress.openPorts:
             print("8080      open    whoknows")
+        if "ssh" in adress.openPorts:
+            print("22        open    ssh")
     else:
         print("Nmap done: 1 IP addresses (0 hosts up) scanned in " + libtvs.toFixed(random.random(), 3) + " seconds")
+
+def ssh(ip):
+    obj = None
+    print("Connecting to " + ip + " on port 22")
+
+    for adress in adresses:
+        if ip == adress.ip and "ssh" in adress.openPorts:
+            obj = adress
+    
+    time.sleep(3)
+
+    if obj != None:
+        if input("root@" + obj.hostname + " password: ") == obj.rootPasswd:
+            time.sleep(1)
+            print("Starting shell")
+        else:
+            time.sleep(1)
+            print("Incorrect password for root@" + obj.hostname)
+    else:
+        print("Cant resolve host " + ip)
